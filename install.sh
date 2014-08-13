3d3a9fd497b25d96825f4199ec255a60648ef564
Lorenzo <fontanalorenzo@me.com>
2014-08-14 01:59:41 +0200

    Git configuration

--- install.sh
+++ install.sh
@@ -7,10 +7,24 @@ if [ "$1" != "--force" ]; then
     fi
 fi
 
+echo "Type your full name followed by enter:"
+read name
+
+echo "Type your email address followed by enter:"
+read email
+
+# Configuring git
+git config --global user.name $name
+git config --global user.email $email
+git config --global core.excludesfile ~/.gitignore_global
+git config --global branch.autosetuprebase always
+
 rm -Rf $HOME/.dotfiles
 git clone https://github.com/fntlnz/dotfiles $HOME/.dotfiles
 ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/scripts/* /usr/local/bin
+ln -s $HOME/.dotfiles/git/.gitignore_global $HOME/.gitignore_global
+
 touch $HOME/.zshrc_local
