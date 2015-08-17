1ff076dfb8971b86c172452eefbbbfb862a264a7
Lorenzo Fontana <lorenzo.fontana@facile.it>
2015-08-17 11:12:12 +0200

    nvimrc

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ ln -sf $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/git/.gitignore_global $HOME/.gitignore_global
 ln -sf $HOME/.dotfiles/gdb/.gdbinit $HOME/.gdbinit
 ln -sf $HOME/.dotfiles/ctags/.ctags $HOME/.ctags
-mkdir -p $HOME/.nvim/autoload
+ln -sf $HOME/.dotfiles/nvim/.nvimrc $HOME/.nvimrc
+ln -sf $HOME/.dotfiles/nvim/autoload/* $HOME/.nvim/autoload
 ln -sf $HOME/.dotfiles/nvim/autoload/* $HOME/.nvim/autoload
 mkdir -p $HOME/.nvim/ftplugin
 ln -sf $HOME/.dotfiles/nvim/ftplugin/* $HOME/.nvim/ftplugin
