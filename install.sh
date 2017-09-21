d14bcb10ad6acafd83c1d23296ae82ab7fd99801
Lorenzo Fontana <lo@linux.com>
2017-09-22 00:36:15 +0200

    Restored vscode config
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ install_path=$HOME/.dotfiles
 git=$(which git)
 stow=$(which stow)
 
-$git clone https://github.com/fntlnz/dotfiles $install_path
+#$git clone https://github.com/fntlnz/dotfiles $install_path
 
 pushd $install_path
 $stow nvim
@@ -22,3 +22,16 @@ $stow screenlayout
 $stow zsh
 $stow vscode
 popd
+
+
+# Code setup
+code --install-extension donjayamanne.python
+code --install-extension   eamodio.gitlens
+code --install-extension   lukehoban.Go
+code --install-extension   ms-vscode.cpptools
+code --install-extension   onsi.vscode-ginkgo
+code --install-extension   redhat.java
+code --install-extension   twxs.cmake
+code --install-extension   vector-of-bool.cmake-tools
+code --install-extension   vscodevim.vim
+code --install-extension   zxh404.vscode-proto3
