f64482b6adb0761442db002533101be1d7979754
Lorenzo Fontana <lo@linux.com>
2017-09-22 00:52:22 +0200

    Editorconfig
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ install_path=$HOME/.dotfiles
 
 git=$(which git)
 stow=$(which stow)
+code=$(which code)
 
-#$git clone https://github.com/fntlnz/dotfiles $install_path
+$git clone https://github.com/fntlnz/dotfiles $install_path
 
 pushd $install_path
 $stow nvim
@@ -25,13 +26,15 @@ popd
 
 
 # Code setup
-code --install-extension donjayamanne.python
-code --install-extension   eamodio.gitlens
-code --install-extension   lukehoban.Go
-code --install-extension   ms-vscode.cpptools
-code --install-extension   onsi.vscode-ginkgo
-code --install-extension   redhat.java
-code --install-extension   twxs.cmake
-code --install-extension   vector-of-bool.cmake-tools
-code --install-extension   vscodevim.vim
-code --install-extension   zxh404.vscode-proto3
+$code --install-extension donjayamanne.python
+$code --install-extension eamodio.gitlens
+$code --install-extension lukehoban.Go
+$code --install-extension ms-vscode.cpptools
+$code --install-extension onsi.vscode-ginkgo
+$code --install-extension redhat.java
+$code --install-extension twxs.cmake
+$code --install-extension vector-of-bool.cmake-tools
+$code --install-extension vscodevim.vim
+$code --install-extension zxh404.vscode-proto3
+$code --install-extension EditorConfig.EditorConfig
+
