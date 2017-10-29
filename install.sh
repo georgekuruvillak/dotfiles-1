69aa905f7794f0f4175e1a440d5fd6f2a6bc2d5f
Lorenzo Fontana <lo@linux.com>
2017-10-29 22:05:37 +0100

    Installing all the things
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ install_path=$HOME/.dotfiles
 
 git=$(which git)
 stow=$(which stow)
+code=$(which code)
 
 if [ ! -d "$install_path" ]; then
   $git clone https://github.com/fntlnz/dotfiles $install_path
@@ -21,6 +22,18 @@ $stow i3
 $stow screenlayout
 $stow dunst
 $stow gnupg
+$stow vscode
 popd
 
-
+# Code setup
+$code --install-extension  donjayamanne.python
+$code --install-extension  eamodio.gitlens
+$code --install-extension  lukehoban.Go
+$code --install-extension  ms-vscode.cpptools
+$code --install-extension  onsi.vscode-ginkgo
+$code --install-extension  redhat.java
+$code --install-extension  twxs.cmake
+$code --install-extension  vector-of-bool.cmake-tools
+$code --install-extension  vscodevim.vim
+$code --install-extension  zxh404.vscode-proto3
+$code --install-extension  dracula-theme.theme-dracula
