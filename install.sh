93b8930f682263d44d0b1269d1638edc54d8dbfb
Lorenzo Fontana <lo@linux.com>
2017-12-02 12:11:36 +0100

    Restored code preferences
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ install_path=$HOME/.dotfiles
 git=$(which git)
 stow=$(which stow)
 curl=$(which curl)
+code=$(which code)
 
 if [ ! -d "$install_path" ]; then
   $git clone https://github.com/fntlnz/dotfiles $install_path
@@ -25,3 +26,14 @@ $stow dunst
 $stow pam
 $stow systemd
 popd
+
+$code --install-extension eamodio.gitlens
+$code --install-extension lukehoban.Go
+$code --install-extension ms-vscode.cpptools
+$code --install-extension onsi.vscode-ginkgo
+$code --install-extension redhat.java
+$code --install-extension twxs.cmake
+$code --install-extension vector-of-bool.cmake-tools
+$code --install-extension vscodevim.vim
+$code --install-extension zxh404.vscode-proto3
+$code --install-extension dracula-theme.theme-dracula
