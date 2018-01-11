a73aa092b93fec8f5839e21b490051bde421ed43
Lorenzo Fontana <lo@linux.com>
2018-01-11 14:12:21 +0100

    Remove fish
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ fi
 
 pushd $install_path
 $curl -L -o gdb/.gdbinit https://git.io/.gdbinit
-$curl -L -o fish/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 $stow nvim
 $stow zsh
 $stow tmux
@@ -26,7 +25,15 @@ $stow i3
 $stow dunst
 $stow pam
 $stow systemd
-$stow fish
+$stow vscode
 popd
 
-fish -c fisher
+$code --install-extension eamodio.gitlens
+$code --install-extension lukehoban.Go
+$code --install-extension ms-vscode.cpptools
+$code --install-extension onsi.vscode-ginkgo
+$code --install-extension redhat.java
+$code --install-extension twxs.cmake
+$code --install-extension vector-of-bool.cmake-tools
+$code --install-extension zxh404.vscode-proto3
+$code --install-extension dracula-theme.theme-dracula
