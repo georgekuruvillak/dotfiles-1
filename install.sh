b0f614e12e374e8d08a0b5b2f04338e315125d60
Lorenzo Fontana <fontanalorenz@gmail.com>
2018-01-27 01:29:43 -0800

    Like cleaning
    Signed-off-by: Lorenzo Fontana <fontanalorenz@gmail.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ install_path=$HOME/.dotfiles
 git=$(which git)
 stow=$(which stow)
 curl=$(which curl)
-code=$(which code)
 
 if [ ! -d "$install_path" ]; then
   $git clone https://github.com/fntlnz/dotfiles $install_path
@@ -33,15 +32,5 @@ $stow i3
 $stow dunst
 $stow pam
 $stow systemd
-$stow vscode
+$stow background
 popd
-
-$code --install-extension eamodio.gitlens
-$code --install-extension lukehoban.Go
-$code --install-extension ms-vscode.cpptools
-$code --install-extension onsi.vscode-ginkgo
-$code --install-extension redhat.java
-$code --install-extension twxs.cmake
-$code --install-extension vector-of-bool.cmake-tools
-$code --install-extension zxh404.vscode-proto3
-$code --install-extension dracula-theme.theme-dracula
