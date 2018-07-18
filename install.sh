ccd832e5e03eb2025e3c2e75f20a88f7679f6b4e
Lorenzo Fontana <lo@linux.com>
2018-07-18 10:14:41 +0200

    i3 settings
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ install_path=$HOME/.dotfiles
 git=$(which git)
 stow=$(which stow)
 curl=$(which curl)
+code=$(which code)
 
 if [ ! -d "$install_path" ]; then
   $git clone https://github.com/fntlnz/dotfiles $install_path
@@ -35,5 +36,17 @@ $stow user-dirs
 $stow i3
 $stow dunst
 $stow screenlayout
+$stow vscode
 popd
 
+$code --install-extension eamodio.gitlens
+$code --install-extension ms-vscode.Go
+$code --install-extension ms-vscode.cpptools
+$code --install-extension vscjava.vscode-java-pack
+$code --install-extension twxs.cmake
+$code --install-extension vector-of-bool.cmake-tools
+$code --install-extension zxh404.vscode-proto3
+$code --install-extension ms-python.python
+$code --install-extension rust-lang.rust
+$code --install-extension devondcarew.bazel-code
+$code --install-extension vscodevim.vim
